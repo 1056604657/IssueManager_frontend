76d4693c067750ca657ab6cd63e295e0346f726b
高伟 <lovely321@aliyun.com>
2024-01-21 20:04:28 +0800

    更新密码、密码复杂度去掉

--- src/views/system/user/api.ts
+++ src/views/system/user/api.ts
@@ -1,58 +1,66 @@
-import { request,downloadFile } from '/@/utils/service';
+import { request, downloadFile } from '/@/utils/service';
 import { PageQuery, AddReq, DelReq, EditReq, InfoReq } from '@fast-crud/fast-crud';
 
 export const apiPrefix = '/api/system/user/';
 
 export function GetDept(query: PageQuery) {
-    return request({
-        url: "/api/system/dept/dept_lazy_tree/",
-        method: 'get',
-        params: query,
-    });
+  return request({
+    url: "/api/system/dept/dept_lazy_tree/",
+    method: 'get',
+    params: query,
+  });
 }
 
 export function GetList(query: PageQuery) {
-    return request({
-        url: apiPrefix,
-        method: 'get',
-        params: query,
-    });
+  return request({
+    url: apiPrefix,
+    method: 'get',
+    params: query,
+  });
 }
 export function GetObj(id: InfoReq) {
-    return request({
-        url: apiPrefix + id,
-        method: 'get',
-    });
+  return request({
+    url: apiPrefix + id,
+    method: 'get',
+  });
 }
 
 export function AddObj(obj: AddReq) {
-    return request({
-        url: apiPrefix,
-        method: 'post',
-        data: obj,
-    });
+  return request({
+    url: apiPrefix,
+    method: 'post',
+    data: obj,
+  });
 }
 
 export function UpdateObj(obj: EditReq) {
-    return request({
-        url: apiPrefix + obj.id + '/',
-        method: 'put',
-        data: obj,
-    });
+  return request({
+    url: apiPrefix + obj.id + '/',
+    method: 'put',
+    data: obj,
+  });
 }
 
 export function DelObj(id: DelReq) {
-    return request({
-        url: apiPrefix + id + '/',
-        method: 'delete',
-        data: { id },
-    });
+  return request({
+    url: apiPrefix + id + '/',
+    method: 'delete',
+    data: { id },
+  });
 }
 
-export function exportData(params:any){
-    return downloadFile({
-        url: apiPrefix + 'export_data/',
-        params: params,
-        method: 'get'
-    })
+export function exportData(params: any) {
+  return downloadFile({
+    url: apiPrefix + 'export_data/',
+    params: params,
+    method: 'get'
+  })
+}
+
+export function resetPwd(id: number, data: { [key: string]: string }) {
+  return request({
+    url: `/api/system/user/${id}/reset_password/`,
+    method: 'put',
+    data,
+  });
 }
--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -7,21 +7,40 @@
             部门列表
             <el-tooltip effect="dark" :content="content" placement="right">
               <el-icon>
-                <QuestionFilled/>
+                <QuestionFilled />
               </el-icon>
             </el-tooltip>
           </p>
-          <el-input v-model="filterText" :placeholder="placeholder"/>
-          <el-tree ref="treeRef" class="font-mono font-bold leading-6 text-7xl" :data="data" :props="treeProps"
-                   :filter-node-method="filterNode" icon="ArrowRightBold" :indent="38" highlight-current @node-click="onTreeNodeClick">
+          <el-input v-model="filterText" :placeholder="placeholder" />
+          <el-tree
+            ref="treeRef"
+            class="font-mono font-bold leading-6 text-7xl"
+            :data="data"
+            :props="treeProps"
+            :filter-node-method="filterNode"
+            icon="ArrowRightBold"
+            :indent="38"
+            highlight-current
+            @node-click="onTreeNodeClick"
+          >
             <template #default="{ node, data }">
-              <element-tree-line :node="node" :showLabelLine="false" :indent="32">
-					<span v-if="data.status" class="text-center font-black font-normal">
-						<SvgIcon name="iconfont icon-shouye" color="var(--el-color-primary)"/>&nbsp;{{ node.label }}
-					</span>
-                <span v-else color="var(--el-color-primary)"> <SvgIcon name="iconfont icon-shouye"/>&nbsp;{{
-                    node.label
-                  }} </span>
+              <element-tree-line
+                :node="node"
+                :showLabelLine="false"
+                :indent="32"
+              >
+                <span
+                  v-if="data.status"
+                  class="text-center font-black font-normal"
+                >
+                  <SvgIcon
+                    name="iconfont icon-shouye"
+                    color="var(--el-color-primary)"
+                  />&nbsp;{{ node.label }}
+                </span>
+                <span v-else color="var(--el-color-primary)">
+                  <SvgIcon name="iconfont icon-shouye" />&nbsp;{{ node.label }}
+                </span>
               </element-tree-line>
             </template>
           </el-tree>
@@ -37,19 +56,52 @@
         </el-card>
       </el-col>
     </el-row>
-
+    <el-dialog
+      v-model="resetPwdVisible"
+      title="重设密码"
+      width="400px"
+      draggable
+      :before-close="handleResetPwdClose"
+    >
+      <div>
+        <el-input
+          v-model="resetPwdFormState.newPassword"
+          type="password"
+          placeholder="请输入密码"
+          show-password
+          style="margin-bottom: 20px"
+        />
+        <el-input
+          v-model="resetPwdFormState.newPassword2"
+          type="password"
+          placeholder="请再次输入密码"
+          show-password
+        />
+      </div>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="handleResetPwdClose">取消</el-button>
+          <el-button type="primary" @click="handleResetPwdSubmit">
+            保存
+          </el-button>
+        </span>
+      </template>
+    </el-dialog>
   </fs-page>
 </template>
 
 <script lang="ts" setup name="user">
-import {useExpose, useCrud} from '@fast-crud/fast-crud';
-import {createCrudOptions} from './crud';
-import * as api from './api';
-import {ElTree} from 'element-plus';
-import {ref, onMounted, watch, toRaw, h} from 'vue';
-import XEUtils from 'xe-utils';
-import {getElementLabelLine} from 'element-tree-line';
-import importExcel from '/@/components/importExcel/index.vue'
+import { useExpose, useCrud } from "@fast-crud/fast-crud";
+import { createCrudOptions } from "./crud";
+import * as api from "./api";
+import { ElTree } from "element-plus";
+import { ref, onMounted, watch, toRaw, h, reactive } from "vue";
+import XEUtils from "xe-utils";
+import { getElementLabelLine } from "element-tree-line";
+import importExcel from "/@/components/importExcel/index.vue";
+import { warningNotification, successNotification } from "/@/utils/message";
+import { resetPwd } from "./api";
+import { Md5 } from "ts-md5";
 
 const ElementTreeLine = getElementLabelLine(h);
 
@@ -67,14 +119,14 @@ interface APIResponseData {
 }
 
 // 引入组件
-const placeholder = ref('请输入部门名称');
-const filterText = ref('');
+const placeholder = ref("请输入部门名称");
+const filterText = ref("");
 const treeRef = ref<InstanceType<typeof ElTree>>();
 
 const treeProps = {
-  children: 'children',
-  label: 'name',
-  icon: 'icon',
+  children: "children",
+  label: "name",
+  icon: "icon",
 };
 
 watch(filterText, (val) => {
@@ -96,8 +148,8 @@ const getData = () => {
   api.GetDept({}).then((ret: APIResponseData) => {
     const responseData = ret.data;
     const result = XEUtils.toArrayTree(responseData, {
-      parentKey: 'parent',
-      children: 'children',
+      parentKey: "parent",
+      children: "children",
       strict: true,
     });
 
@@ -107,25 +159,66 @@ const getData = () => {
 
 //树形点击事件
 const onTreeNodeClick = (node: any) => {
-  const {id} = node;
-  crudExpose.doSearch({form: {dept: id}});
+  const { id } = node;
+  crudExpose.doSearch({ form: { dept: id } });
 };
 
 // 页面打开后获取列表数据
 onMounted(() => {
   getData();
 });
+let resetPwdVisible = ref(false);
+let resetPwdFormState = reactive({
+  id: 0,
+  newPassword: "",
+  newPassword2: "",
+});
+const handleResetPwdOpen = ({ id }: { id: number }) => {
+  resetPwdFormState.id = id;
+  resetPwdVisible.value = true;
+};
+const handleResetPwdClose = () => {
+  resetPwdVisible.value = false;
+  resetPwdFormState.id = 0;
+  resetPwdFormState.newPassword = "";
+  resetPwdFormState.newPassword2 = "";
+};
+const handleResetPwdSubmit = async () => {
+  if (!resetPwdFormState.id) {
+    warningNotification("请选择用户！");
+    return;
+  }
+  if (!resetPwdFormState.newPassword || !resetPwdFormState.newPassword2) {
+    warningNotification("请输入密码！");
+    return;
+  }
+  if (resetPwdFormState.newPassword !== resetPwdFormState.newPassword2) {
+    warningNotification("两次输入密码不一致");
+    return;
+  }
+  const res = await resetPwd(resetPwdFormState.id, {
+    newPassword: Md5.hashStr(resetPwdFormState.newPassword),
+    newPassword2: Md5.hashStr(resetPwdFormState.newPassword2),
+  });
 
+  if (res?.code === 2000) {
+    successNotification(res.msg || "修改成功！");
+    handleResetPwdClose();
+  }
+};
 // crud组件的ref
 const crudRef = ref();
 // crud 配置的ref
 const crudBinding = ref();
 // 暴露的方法
-const {crudExpose} = useExpose({crudRef, crudBinding});
+const { crudExpose } = useExpose({ crudRef, crudBinding });
 // 你的crud配置
-const {crudOptions} = createCrudOptions({crudExpose});
+const { crudOptions } = createCrudOptions({
+  crudExpose,
+  context: { handleResetPwdOpen },
+});
 // 初始化crud配置
-const {resetCrudOptions} = useCrud({crudExpose, crudOptions});
+const { resetCrudOptions } = useCrud({ crudExpose, crudOptions });
 
 // 页面打开后获取列表数据
 onMounted(() => {
@@ -147,6 +240,7 @@ onMounted(() => {
 }
 
 .font-normal {
-  font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, SimSun, sans-serif;
+  font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB,
+    Microsoft YaHei, SimSun, sans-serif;
 }
 </style>
